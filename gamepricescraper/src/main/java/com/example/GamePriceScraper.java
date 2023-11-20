4ed0bceb0f2f9364875034068a9c0cf5337bfbb3
Joomun <joomunmuddathir@gmail.com>
2023-11-20 22:16:13 +0400

    Scrape data Dynaic Still buggy

--- gamepricescraper/src/main/java/com/example/GamePriceScraper.java
+++ gamepricescraper/src/main/java/com/example/GamePriceScraper.java
@@ -5,11 +5,21 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 
 public class GamePriceScraper {
 	
     public static void main(String[] args) {
+    	
+    	
+    	WebDriverManager.chromedriver().browserVersion("119.0.6045.160").setup();
         scrapeSteam();
         scrapeGOG();
         scrapeK4g();
@@ -17,16 +27,64 @@ public class GamePriceScraper {
     }
 
     private static void scrapeSteam() {
-        // Replace with the actual URL and CSS selectors for Steam
-        scrapeWebsite("https://store.steampowered.com/app/1551360/Forza_Horizon_5/", "div.game_purchase_price.price[data-price-final]");
+        String gameUrl = "https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/";
+        
+        try {
+            Document gamePage = Jsoup.connect(gameUrl)
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+                .referrer("http://www.google.com")
+                .timeout(10 * 1000)
+                .get();
+
+            // Select the container that includes both the title and the price
+            Elements gameEditions = gamePage.select("div.game_area_purchase_game");
+
+            for (Element edition : gameEditions) {
+                // Extract the title
+                String title = edition.select("h1").text();
+
+                // Extract the price string
+                String priceText = edition.select("div.game_purchase_price").text();
+
+                // Check if the price string is not empty
+                if (!priceText.isEmpty()) {
+                    System.out.println("Game: " + title + " - Price: " + priceText);
+                } else {
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void scrapeGOG() {
-        // Replace with the actual URL and CSS selectors for GOG
-        scrapeWebsite("https://www.gog.com/", "CSS_SELECTOR_FOR_GOG");
+        // Set up WebDriverManager to download and set up the ChromeDriver binary
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Run in headless mode (no browser UI)
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://www.gog.com/en/game/the_witcher_3_wild_hunt_game_of_the_year_edition");
+
+            // Wait for the price element to be present on the page
+            WebElement priceElement = driver.findElement(By.cssSelector("span.product-actions-price__final-amount"));
+
+            // Get the text of the element, which should contain the price
+            String price = priceElement.getText();
+
+            // Print the price
+            System.out.println("Price: " + price);
+        } catch (Exception e) {
+            System.err.println("Error scraping GOG: " + e.getMessage());
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
     }
-
-
+    
 
     private static void scrapeK4g() {
         // Replace with the actual URL and CSS selectors for K4g
